44731860592fc868a85f3e152eb632d37456cc33
Juninho <juninhowyx@gmail.com>
2025-10-02 19:55:23 -0500

    add splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    //splash screen
+    implementation(libs.androidx.core.splashscreen)
 }
