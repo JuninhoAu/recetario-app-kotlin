f2c5860432a61d8db6729f51b045f90f72af4f44
Juninho <juninhowyx@gmail.com>
2025-10-04 02:49:52 -0500

    add query dao

--- app/src/main/java/com/juni/recetarioapp/data/local/database/RecipeDao.kt
+++ app/src/main/java/com/juni/recetarioapp/data/local/database/RecipeDao.kt
@@ -4,11 +4,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RecipeDao {
 
+    @Query("SELECT * FROM recipes ORDER BY favorito DESC, nombre ASC")
+    fun getAllRecipes(): Flow<List<RecipeEntity>>
+
     @Insert
     suspend fun insertRecipe(recipe: RecipeEntity)
+
+    @Update
+    suspend fun updateRecipe(recipe: RecipeEntity)
 
 }
